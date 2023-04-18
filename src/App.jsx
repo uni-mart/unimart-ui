c9b0917654860642b2f6960cf9b29d76816638d4
Utsab Adhikari <48797078+autsav@users.noreply.github.com>
2023-04-18 17:18:51 +0100

    Uni 8: Login Page completion (#1)
    * UNI-5: Added required pachages and config files
    
    * UNI-8: Completion of Login Page
    
    ---------
    
    Co-authored-by: bitcode001 <anil.rai00101@gmail.com>
    Co-authored-by: autsav <autsav73@.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,35 @@
+import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Routes,Route,Navigate } from 'react-router-dom'
+
 import { useState } from 'react'
+import LandingPage from './Pages/LandingPage'
+import LoginPage from 'Pages/LoginPage'
+import SignUpPage from 'Pages/SignUpPage'
+import Header from 'components/Header'
+import Footer from 'components/Footer'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Unimart UI</h1>
+    <div>
+    <Router>
+        <Header />
+          <main className='py-3'>
+          <Container >
+              <Routes>
+                <Route path='/' element={<LandingPage />} exact />
+                <Route path='/login' element={<LoginPage />}  />
+                <Route path='/signUp' element={<SignUpPage />}  />
+
+              </Routes>
+              
+          </Container>
+            
+          </main>
+        <Footer />
+    </Router>
+    </div>
+    
   )
 }
 
