b3ef18ee2bb65af718635db52ace7d6b48cf70be
Anil Rai <82773560+bitcode001@users.noreply.github.com>
2023-04-18 17:44:16 +0100

    Uni 9: Cosmetic updates on login and register page (#2)
    * UNI-5: Added required pachages and config files
    
    * UNI-8: Completion of Login Page
    
    * UNI-9: Completion of Signup Page
    
    ---------
    
    Co-authored-by: autsav <autsav73@.com>

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -9,53 +9,102 @@ import { login } from '../Actions/UserActions'
 
 
 const SignUpPage = () => {
+    const [studentId, setStudentId] = useState('')
+    const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
 
     const submitHandler = (e) => {
         e.preventDefault()
         dispatch(login(email, password))
     }
 
-  return (
-    <FormContainer>
-        <h1>Sign In</h1>
-        <Form onSubmit={submitHandler}>
-            <Form.Group controlId='email'>
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control
-                    type='email'
-                    placeholder='Enter Email'
-                    value={email}
-                    onChange={(e) =>setEmail(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-            <Form.Group controlId='password'>
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                    type='password'
-                    placeholder='Enter Password'
-                    value={email}
-                    onChange={(e) =>setPassword(e.target.value)}
-                >
-                </Form.Control>
-            </Form.Group>
-            <Button  type='submit' variant="primary">
-                Sign In
+    return (
+        <FormContainer>
+            <h1>Register</h1>
+            {/* {message && <Message variant={'danger'} >{message}</Message>}
+
+           {error && <Message variant={'danger'} >{error}</Message>}
+           {loading && <Loader />} */}
+            <Form onSubmit={submitHandler}>
+                <Form.Group controlId='studentId' className='mb-2'>
+                    <Form.Label>Student</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder='Enter Student ID'
+                        value={studentId}
+                        onChange={(e) => setStudentId(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='name' className='mb-2'>
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control
+                        required
+                        type="text"
+                        placeholder='Enter Name'
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='email' className='mb-2'>
+                    <Form.Label>Student Email</Form.Label>
+                    <Form.Control
+                        required
+                        type='email'
+                        placeholder='Enter Email'
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='password' className='mb-2'>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Enter Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId='confirmPassword' className='mb-2'>
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control
+                        required
+                        type='password'
+                        placeholder='Enter Confirm Password'
+                        value={confirmPassword}
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    >
+                    </Form.Control>
+                </Form.Group>
+
+                <Button type='submit' variant="primary" className='mt-4'>
+                    Register
                 </Button>
-                </Form>
-                <Row className='py-3' >
+
+            </Form>
+            <Row className='py-3' >
                 <Col>
-                    New Customer ? <Link
-                        to='/signUp'>
-                        Sign Up
+                    Already Registered ? <Link
+                        to='/login'>
+                        Log In
                     </Link>
                 </Col>
 
             </Row>
-    </FormContainer>
-  )
+        </FormContainer>
+    )
 }
 
 export default SignUpPage
